cf98ff9418a9d2f869797d538eda704efc1679dc
Kaua2123 <kauantnetsoares@gmail.com>
2024-08-22 10:40:43 -0300

    fix: fixed type errors in placeholder-data.ts

--- src/app/lib/placeholder-data.ts
+++ src/app/lib/placeholder-data.ts
@@ -3,7 +3,7 @@ import { Task } from './definitions';
 // arquivo de dados ficticios enqunato as funcionalidades fullstack não estão prontas
 export const tasks: Task[] = [
   {
-    id: 1,
+    id: '1',
     name: 'Reunião com cliente',
     description: 'Reunião para discutir os requisitos do novo projeto',
     tags: 'trabalho',
@@ -12,7 +12,7 @@ export const tasks: Task[] = [
     updated_at: '2024-08-17T10:00:00Z',
   },
   {
-    id: 2,
+    id: '2',
     name: 'Planejar festa de aniversário',
     description: 'Organizar os detalhes da festa de aniversário do João',
     tags: 'pessoal',
@@ -21,7 +21,7 @@ export const tasks: Task[] = [
     updated_at: '2024-08-16T15:00:00Z',
   },
   {
-    id: 3,
+    id: '3',
     name: 'Publicar post no LinkedIn',
     description: 'Escrever e publicar um post sobre o novo projeto no LinkedIn',
     tags: 'social',
@@ -30,7 +30,7 @@ export const tasks: Task[] = [
     updated_at: '2024-08-15T15:30:00Z',
   },
   {
-    id: 4,
+    id: '4',
     name: 'Revisar código',
     description:
       'Revisar o código da nova funcionalidade antes de fazer o merge',
@@ -40,7 +40,7 @@ export const tasks: Task[] = [
     updated_at: '2024-08-17T09:00:00Z',
   },
   {
-    id: 5,
+    id: '5',
     name: 'Marcar jantar com amigos',
     description:
       'Reservar um restaurante para jantar com os amigos na sexta-feira',
@@ -50,7 +50,7 @@ export const tasks: Task[] = [
     updated_at: '2024-08-12T14:00:00Z',
   },
   {
-    id: 6,
+    id: '6',
     name: 'Atualizar perfil no LinkedIn',
     description:
       'Adicionar novas certificações e experiências no perfil do LinkedIn',
@@ -60,7 +60,7 @@ export const tasks: Task[] = [
     updated_at: '2024-08-18T10:00:00Z',
   },
   {
-    id: 7,
+    id: '7',
     name: 'Preparar apresentação de projeto',
     description:
       'Criar slides para apresentação do projeto na reunião de sexta-feira',
@@ -70,7 +70,7 @@ export const tasks: Task[] = [
     updated_at: '2024-08-17T15:00:00Z',
   },
   {
-    id: 8,
+    id: '8',
     name: 'Comprar presente de aniversário',
     description: 'Escolher e comprar um presente para o aniversário da Ana',
     tags: 'pessoal',
@@ -79,7 +79,7 @@ export const tasks: Task[] = [
     updated_at: '2024-08-16T12:00:00Z',
   },
   {
-    id: 9,
+    id: '9',
     name: 'Participar de webinar sobre tecnologia',
     description: 'Assistir ao webinar sobre as novas tendências em tecnologia',
     tags: 'trabalho',
@@ -88,7 +88,7 @@ export const tasks: Task[] = [
     updated_at: '2024-08-15T11:00:00Z',
   },
   {
-    id: 10,
+    id: '10',
     name: 'Planejar viagem para o fim de semana',
     description:
       'Organizar a viagem para a casa de campo no próximo fim de semana',
@@ -98,7 +98,7 @@ export const tasks: Task[] = [
     updated_at: '2024-08-14T16:00:00Z',
   },
   {
-    id: 11,
+    id: '11',
     name: 'Reunião de equipe',
     description: 'Discussão sobre o progresso do projeto com a equipe',
     tags: 'trabalho',
@@ -107,7 +107,7 @@ export const tasks: Task[] = [
     updated_at: '2024-08-13T10:00:00Z',
   },
   {
-    id: 12,
+    id: '12',
     name: 'Criar planejamento de marketing',
     description: 'Desenvolver um plano de marketing para o próximo trimestre',
     tags: 'trabalho',
@@ -116,7 +116,7 @@ export const tasks: Task[] = [
     updated_at: '2024-08-12T13:00:00Z',
   },
   {
-    id: 13,
+    id: '13',
     name: 'Organizar festa de fim de ano',
     description:
       'Preparar todos os detalhes para a festa de fim de ano da empresa',
@@ -126,7 +126,7 @@ export const tasks: Task[] = [
     updated_at: '2024-08-11T18:00:00Z',
   },
   {
-    id: 14,
+    id: '14',
     name: 'Configurar nova estação de trabalho',
     description: 'Montar e configurar o novo equipamento de trabalho',
     tags: 'trabalho',
@@ -135,7 +135,7 @@ export const tasks: Task[] = [
     updated_at: '2024-08-10T10:00:00Z',
   },
   {
-    id: 15,
+    id: '15',
     name: 'Estudar para o exame de certificação',
     description:
       'Revisar material e fazer exercícios para o exame de certificação',
