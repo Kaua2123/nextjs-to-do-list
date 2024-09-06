687fdd930626fa339b4c97a985942524996a8309
Kaua2123 <kauantnetsoares@gmail.com>
2024-09-06 12:13:17 -0300

    feat: add 'authorized' middleware

--- auth.config.ts
+++ auth.config.ts
@@ -6,7 +6,18 @@ export const authConfig = {
   },
   // middlewares
   callbacks: {
-    // authorized({ auth, request: { nextUrl } }) {},
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnTasksPage = nextUrl.pathname.startsWith('/tasks');
+      if (isOnTasksPage) {
+        if (isLoggedIn) return true; // se estiver logado, retoran true. pode ficar na pagina.
+        return false; // por padrão, retorna falso, pois não está logado.
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/tasks', nextUrl));
+      }
+
+      return true;
+    },
   },
   // diferentes opções de login
   providers: [],
