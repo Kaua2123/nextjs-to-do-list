cb47374307d51a6a3d540a3d1b7265c9f3168b00
Kaua2123 <kauantnetsoares@gmail.com>
2024-09-10 10:42:44 -0300

    feat: add styles to turn homepage responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,12 @@ import { ArrowRight } from 'lucide-react';
 export default function Page() {
   return (
     <div>
-      <div className="bg-green-600 mb-28 p-28 rounded-3xl m-8 flex justify-center items-center">
+      <div className="bg-green-600 mb-28 p-10 md:p-28 rounded-3xl m-8 flex justify-center items-center">
         <p className="text-white font-bold text-7xl">To-Do List</p>
       </div>
-      <div
-        style={{ width: '120rem', margin: '0 auto' }}
-        className="flex items-center justify-between"
-      >
-        <div className="flex flex-col items-start gap-8">
-          <p className="text-7xl  leading-relaxed">
+      <div className="flex md:flex-row md:mx-52 flex-col items-center justify-between">
+        <div className="flex flex-col items-center md:items-start gap-8">
+          <p className="md:text-7xl text-5xl md:text-start text-center md:leading-relaxed">
             Seu app de <br /> lista de tarefas.
           </p>
           <Link href="/login">
@@ -32,6 +29,15 @@ export default function Page() {
             width={650}
             height={250}
             alt="Illustrative image"
+            className="hidden md:block"
+          />
+
+          <Image
+            src="/to-do-list.svg"
+            width={300}
+            height={420}
+            alt="Illustrative image"
+            className="mt-20 block md:hidden"
           />
         </div>
       </div>
--- src/app/ui/button.tsx
+++ src/app/ui/button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export function Button({ children, className }: ButtonProps) {
   return (
     <button
-      className={`${className} bg-green-600 border-none rounded-lg text-white p-6 px-10 text-3xl hover:bg-green-500 transition-all flex items-center justify-center gap-5`}
+      className={`${className} bg-green-600 border-none rounded-lg text-white  p-4 md:p-6 md:px-10 text-xl md:text-3xl hover:bg-green-500 transition-all flex items-center justify-center gap-5`}
     >
       {children}
     </button>
