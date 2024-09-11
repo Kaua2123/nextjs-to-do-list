6f66ffce13ed3a3ebb42fe02351cd9d85435fd43
Kaua2123 <kauantnetsoares@gmail.com>
2024-09-11 14:08:20 -0300

    feat: add styles in login and register pages to make them responsive

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
       <div className="flex items-center justify-center h-screen ">
         <div className="bg-gray-50 rounded-3xl p-10 flex flex-col gap-20">
           <div className="bg-green-600  flex items-center justify-center w-full text-white p-7 px-28 rounded-3xl">
-            <p className="text-7xl">To-do List</p>
+            <p className="text-5xl md:text-7xl">To-do List</p>
           </div>
           <div>
             <LoginForm />
